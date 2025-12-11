3e58ad12e3d34bb924bd664fc61e914acd7f3024
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-11 17:46:33 +0530

    Add logging and cleanup for uploaded files in upload controller

--- src/controllers/upload.controllers.js
+++ src/controllers/upload.controllers.js
@@ -2,18 +2,22 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import fs from "fs";
 
 const uploadFile = asyncHandler(async (req, res) => {
   if (!req.file) {
     throw new ApiError(400, "No file uploaded");
   }
 
+  console.log("Uploaded file info:", req.file); //multer middleware adds the file info to req.file
   const localFilePath = req.file.path;
 
   const cloudinaryUrl = await uploadOnCloudinary(localFilePath);
   if (!cloudinaryUrl) {
     throw new ApiError(500, "File upload to Cloudinary failed");
   }
+
+  fs.unlinkSync(localFilePath); //remove the locally saved temporary file after successful upload to Cloudinary
 
   res
     .status(200)
