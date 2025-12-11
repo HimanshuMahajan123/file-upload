fd0f26006792a652faec11d31b4cfad13b05c044
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2025-12-11 17:32:52 +0530

    Enhance comments in multer.middleware.js for clarity and consistency

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,17 @@
 import multer from "multer";
 import { nanoid } from "nanoid";
 
+//diskStorage tells Multer to store the file on disk (your server) temporarily.
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = nanoid(6);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.fieldname + "-" + uniqueSuffix); //Prevents overwriting files with the same name.
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }); //Create the Multer instance , This upload object has methods like: .single("fieldname")
 
 export default upload;
